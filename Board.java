0f32163d735297a92d7ef2c1b512a2b875bd573c
HaomZhou <47665850+HaomZhou@users.noreply.github.com>
2020-03-16 23:02:33 -0700

    checkEnd

--- Board.java
+++ Board.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
 
 public class Board {
 	int boardWidth;
 	int boardHeight;
-	Tile[][] gameBoard;
+	TFETile[][] gameBoard;
+	ArrayList<TFETile> availableMove;
 	
 	Board(int boardWidth, int boardHeight){
 		this.boardWidth = boardWidth;
 		this.boardHeight = boardHeight;
-		gameBoard = new Tile[boardWidth][boardHeight];
+		gameBoard = new TFETile[boardWidth][boardHeight];
+		availableMove= new ArrayList<TFETile>();
 	}
+	
+	
+	
 	public void printBoard(){
 		for(int row = 0; row < this.boardWidth;row++){
 			for(int col = 0; col < this.boardHeight;col++){
@@ -17,6 +23,25 @@ public class Board {
 				else
 					gameBoard[row][col].printTile();
 			System.out.print('\n');	
+			}
 		}
 	}
+	
+	//check win/lose/continue
+	public boolean checkEnd(){
+		if(availableMove.size()==0) {
+			System.out.println("You Lose");
+			return true;
+		}
+		for(int row = 0; row < this.boardWidth;row++){
+			for(int col = 0; col < this.boardHeight;col++){
+				if(gameBoard[row][col].value==2048){
+					System.out.println("You Win");
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 }
