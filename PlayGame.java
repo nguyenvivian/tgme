d6af77303979c06d14c63428adbcfce78be4cc98
Martin Murata <martinmurata.com>
2020-03-17 13:44:07 -0700

    added UserProfile class

--- PlayGame.java
+++ PlayGame.java
@@ -2,5 +2,29 @@
 public class PlayGame {
 	String gameName;
 	Board gameBoard;
-	Move[][] moveSet;
+	UserProfile[] players;
+	// how should PlayerInput play into PlayGame?
+	
+	public void handlePlayerActions(){
+
+	}
+	public void startGame(){
+
+	}
+	public void handleTurns(){
+
+	}
+	public void startPhase(){
+
+	}
+	public void endPhase(){
+
+	}
+	public void checkPhase(){
+
+	}
+	public void updateScore(){
+
+	}
+	
 }
